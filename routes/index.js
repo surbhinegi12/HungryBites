fb9d4cd4a64c74320e025b9827644cc84a63c1b9
surbhinegi12 <surbhinegi2001@gmail.com>
2023-05-15 12:30:29 +0530

    Modify index.js for routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,25 @@ var router = express.Router();
 
 router.get("/", function (req, res, next) {
   try {
-    res.json({ title: "CRUD ENDPOINT PROJECT" });
+    var users = [
+      "view users (get) route : /users",
+      "login (post) route : /users/login",
+      "signup (post) route : /users/signup",
+      "delete account (delete) route : /users/:id",
+      "logout (post) route : /users/logout",
+      "update user (put) route : /users/:id",
+    ];
+    var products = [
+      "view products (get) route : /products",
+      "add product (post) route : /products/addProduct",
+      "delete product (delete) route : /products/:id",
+    ];
+    var orders = [
+      "view orders (get) route : /orders",
+      "add orders (post) route : /orders/addOrder",
+    ];
+
+    res.status(200).json({users, products, orders});
   } catch (err) {
     res.json(err);
   }
