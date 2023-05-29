5ab940f286ac3916e4130673ec2c590d1c80061d
surbhinegi12 <surbhinegi2001@gmail.com>
2023-05-29 17:29:52 +0530

    Modify products routes

--- routes/products.js
+++ routes/products.js
@@ -3,23 +3,135 @@ var router = express.Router();
 const DB = require("../services/DB");
 const client = require("../utils/elasticsearch");
 
-// app.get("/", (req, res) => {
-//   const search = req.query.text
-//   esClient.search({
-//       index: "products",
-//       body: {
-//           query: {
-//               match: {"name": searchText.trim()}
-//           }
-//       }
-//   })
-//   .then(response => {
-//       return res.json(response)
-//   })
-//   .catch(err => {
-//       return res.status(500).json({"message": "Error"})
-//   })
-// })
+const productMapping = {
+  properties: {
+    name: { type: "text" },
+    price: { type: "double" },
+    units: { type: "integer" },
+  },
+};
+
+const indexName = "products";
+
+async function createProductIndex() {
+  try {
+    const indexExists = await client.indices.exists({ index: indexName });
+    if (indexExists) {
+      console.log(
+        `Index '${indexName}' already exists. Skipping index creation.`
+      );
+      return;
+    }
+
+    const response = await client.indices.create({
+      index: indexName,
+      body: {
+        mappings: productMapping,
+      },
+    });
+
+    console.log(`Index '${indexName}' and mapping created successfully`);
+  } catch (error) {
+    console.error("Error creating index:", error);
+  }
+}
+
+createProductIndex();
+
+router.get("/", async (req, res) => {
+  try {
+    const data = await client.search({
+      index: "products",
+      body: {
+        query: {
+          match_all: {},
+        },
+      },
+    });
+    res
+      .status(200)
+      .json({ products: data.hits.hits.map((hit) => hit._source) });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, price, units } = req.body;
+  try {
+    const [product] = await DB("product")
+      .insert({ name, price, units })
+      .returning("*");
+    product.price = +product.price;
+    client.index({
+      index: "products",
+      id: product.id,
+      body: {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        units: product.units,
+      },
+    });
+
+    res.status(201).json({ product });
+  } catch (err) {
+    console.error("Error:", err);
+    return res.status(500).json({ message: "Error" });
+  }
+});
+
+router.delete("/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+    console.log(productId);
+    await client.delete({
+      index: "products",
+      id: productId,
+    });
+    await DB("product").where({ id: productId }).del();
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Product cannot be deleted" });
+  }
+});
+
+router.put("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const { name, price, units } = req.body;
+    await client.update({
+      index: "products",
+      id: productId,
+      body: {
+        doc: {
+          name,
+          price,
+          units,
+        },
+      },
+    });
+    await DB("product").where({ id: productId }).update({
+      name,
+      price,
+      units,
+    });
+
+    res.status(200).json({ message: "Product updated successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Error" });
+  }
+});
+
+// function validateId(req, res, next) {
+//   const { id } = req.params;
+//   if (!Number.isInteger(Number(id))) {
+//     return res.status(400).send("Invalid id parameter, must be an integer");
+//   }
+//   next();
+// }
 
 // router.get("/", async (req, res) => {
 //   const { id, name, minPrice, maxPrice } = req.query;
@@ -117,40 +229,6 @@ const client = require("../utils/elasticsearch");
 //     }
 //   }
 // });
-
-router.post("/", async (req, res) => {
-  const { name, price, units } = req.body;
-  try {
-    const [product] = await DB("product")
-      .insert({ name, price, units })
-      .returning("*");
-
-    product.price = +product.price;
-    console.log(typeof price);
-    client.index({
-      index: "products",
-      id: product.id,
-      body: {
-        name: product.name,
-        price: product.price,
-        units: product.units,
-      },
-    });
-
-    res.status(201).json({ product });
-  } catch (err) {
-    console.error("Error:", err);
-    return res.status(500).json({ message: "Error" });
-  }
-});
-
-function validateId(req, res, next) {
-  const { id } = req.params;
-  if (!Number.isInteger(Number(id))) {
-    return res.status(400).send("Invalid id parameter, must be an integer");
-  }
-  next();
-}
 
 // router.delete("/:id", validateId, async (req, res) => {
 //   const { id } = req.params;
